a8eddb5832acf6a907a38bb196e3e52b3ddbb033
bmoki <breno26sonda@gmail.com>
2023-05-08 16:54:58 -0300

    styled-components

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Amount,
+  Category,
+  CategoryName,
+  Container,
+  Date,
+  Footer,
+  Icon,
+  Title,
+} from "./styles";
+
+interface Category {
+  name: string;
+  icon: string;
+}
+
+export interface TransactionCardProps {
+  type: "positive" | "negative";
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
+
+interface Props {
+  data: TransactionCardProps;
+}
+
+export function TransactionCard({
+  data: { amount, category, date, title, type },
+}: Props) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Amount type={type}>
+        {type === "negative" && "- "}
+        {amount}
+      </Amount>
+      <Footer>
+        <Category>
+          <Icon name={category.icon} />
+          <CategoryName>{category.name}</CategoryName>
+        </Category>
+        <Date>{date}</Date>
+      </Footer>
+    </Container>
+  );
+}
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,3 +1,8 @@
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/TransactionCard";
 import {
   Container,
   Header,
@@ -8,9 +13,42 @@ import {
   UserName,
   UserContainer,
   Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      amount: "R$ 12.000,00",
+      title: "Site",
+      date: "13/04/2004",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+    },
+    {
+      id: "2",
+      type: "negative",
+      amount: "R$ 20,00",
+      title: "Aluguel",
+      date: "13/04/2004",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag",
+      },
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -25,6 +63,35 @@ export function Dashboard() {
           <Icon name="power" />
         </UserContainer>
       </Header>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="17.400,00"
+          lastTransaction="Ultima entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saidas"
+          amount="17.400,00"
+          lastTransaction="Ultima saida dia 13 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="17.400,00"
+          lastTransaction="01 a 13 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 }
